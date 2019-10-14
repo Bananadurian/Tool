6faebf7971bf2fcf4566bf9ccced3eb6b18cbf5e
qinjinfeng7@win <qinjinfeng7@outlook.com>
2019-10-14 23:20:54 +0800

    优化lieyouInterfaceTool.py

--- interface_tool/LieyouInterfaceTool.py
+++ interface_tool/LieyouInterfaceTool.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 import requests
 import json
+
 class InterfaceTool:
     def __init__(self,bid,password='asd123'):
         self.bid = bid
@@ -15,10 +16,7 @@ class InterfaceTool:
         
     def set_password(self):
         '''设置密码'''
-        #temp = input('bid,password(英文逗号分隔):')
-        #bid = temp.split(',')[0]
         flag = True
-        #print(self.bid,self.password)
         while flag:
             result = self.get_url('http://passport.lieyou.com/lieyou/sdk/create_password?cache=no&bid={}&isNew=1&password={}'.format(self.bid,self.password))
             if result['code'] == 0:
@@ -40,6 +38,7 @@ class InterfaceTool:
         result = self.get_url('http://api.lieyou.com/admin/account_manage/do_relieve_mobile_data?bid={}&relieveReason=test'.format(self.bid))
         print(result['msg'])
     
+
 if __name__=='__main__':
     print('='*35)
     print('{:10} {}'.format('Choose','Content'))
@@ -48,7 +47,7 @@ if __name__=='__main__':
     print(' {:<8}  {}'.format('2','修改账号为测试号'))
     print(' {:<8}  {}'.format('3','清除账号绑定设备'))
     print(' {:<8}  {}'.format('4','设置密码(默认asd123,可选)'))
-    print(' {:<8}{}'.format('其它','输入数字5回车退出'))
+    print(' {:<8}{}'.format('其它','回车直接退出'))
     print('-'*35)
 
     try:
@@ -69,13 +68,13 @@ if __name__=='__main__':
                     tool.set_password()
                 else:
                     print('选项不存在')
-                break
+                #break
             
             elif len(temp)==3 and temp[1].isdigit() and len(temp[1])==11:
                 tool = InterfaceTool(temp[1],temp[2])
                 tool.set_password()   
-                break
-            elif temp[0] == '5':
+                #break
+            elif temp[0] == '':
                 print('退出成功')
                 break
             else:
