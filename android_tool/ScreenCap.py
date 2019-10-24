94057a6567fd2aae7eb302d0361948e2234bfb92
qinjinfeng <qinjinfeng7@outlook.com>
2019-10-25 00:15:09 +0800

    修改截图文件名为当前时间戳

--- android_tool/ScreenCap.py
+++ android_tool/ScreenCap.py
@@ -1,15 +1,19 @@
-#import os
+#! /usr/bin/env python
 import sys
-import android_adb_tool as adb
+#import android_adb_tool as adb
+from android_adb_tool import get_device_state,screencap
+from time import localtime,strftime
 '''
-截图文件名可以通过运行py的时候带参数修改，默认为sceen
+截图文件名可以通过运行py的时候带参数修改，默认为当前时间戳
 如：python *.py sceen1
 '''
-adb.get_device_state()
+
+get_device_state()
 if len(sys.argv)==2:
-    adb.screencap(sys.argv[1])
+    screencap(sys.argv[1])
 else:
-    adb.screencap()
+    file_name = strftime('%Y%m%d%H%M%S',localtime())
+    screencap(file_name)
 
 if __name__=='__main__':
     input('Enter Pass')
