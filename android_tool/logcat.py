2a14f665c2cfc50ec91048d4adfde2c36b36ba67
QJF <qinjinfeng@aipai.com>
2019-11-01 10:07:01 +0800

    新增logcat.py

---
+++ android_tool/logcat.py
@@ -0,0 +1,9 @@
+import os
+from time import strftime,localtime
+
+print('Ctrl+C停止')
+file_name = strftime('%y%m%d%H%M%S',localtime())
+os.system('adb wait-for-device')
+os.system('adb logcat -c')
+os.system(r'adb logcat >C:\Users\Administrator\Desktop\{}.txt'.format(file_name))
+
