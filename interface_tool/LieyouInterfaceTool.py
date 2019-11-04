110f3aa84846e05694d94a9632abeef48e510464
QJF <qinjinfeng@aipai.com>
2019-11-04 21:49:05 +0800

    fix 网络请求超时没有显示错误信息

--- interface_tool/LieyouInterfaceTool.py
+++ interface_tool/LieyouInterfaceTool.py
@@ -10,7 +10,7 @@ class InterfaceTool:
         
     def get_url(self,url):
         headers={'User-Agent':'Mozilla/5.0(iPhone;CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1'}
-        r = requests.get(url=url,headers=headers,timeout=15)
+        r = requests.get(url=url,headers=headers,timeout=30)
         if r.status_code != 200:
             raise Exception('network error:{}'.format(r.status_code))
         return json.loads(r.content)
@@ -86,4 +86,5 @@ if __name__=='__main__':
         #os.system('exit')
     except Exception as e:
         print(e)
+        input('Enter Pass')
     
