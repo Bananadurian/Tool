261fb6da670afd720c21e2b64e6e8ae241005169
qinjinfeng <qinjinfeng7@outlook.com>
2019-11-06 00:10:51 +0800

    更新closeOrder.py

--- interface_tool/closeOrder.py
+++ interface_tool/closeOrder.py
@@ -1,24 +1,38 @@
-# /usr/bin/env python
+#! /usr/bin/env python
 import requests
 import json
 
 def get_url(url):
-    headers = {User-Agent: 'Mozilla/5.0(iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'}
+    headers = {'User-Agent':'Mozilla/5.0(iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'}
     r = requests.get(url = url,headers = headers,timeout = 30)
     if r.status_code != 200:
         raise Exception('network error:{}'.format(r.status_code))
     return json.loads(r.content)
 
+def read_file():
+    with open('1.txt') as f:
+        temp = []
+        id = f.readlines()
+        for id in id:
+            temp.append(id.strip('\n'))
+        orderId = []
+        for temp in temp:
+            if temp != '':
+                orderId.append(temp.strip())
+    return orderId
+
 def run():
-    with open('1.txt','r') as f:
-        while True:
-            orderId = f.readline()
-            if orderId:
-                r = 'http://api.lieyou.com/api/order/testCloseOrder?orderId={}&cache=no'.format(orderId)
-                print(r)
-            else:
-                break
+    orderId = read_file()
+    for orderId in orderId:
+        url = 
+        result = get_url(url)
+        print('orderId:{} result:{}'.format(orderId,result['msg']))
+
 
 if __name__=='__main__':
-    run()
-    
+    try:
+        a = read_file()
+        print(a)
+    except Exception as e:
+        print(e)
+        input('Enter Pass')
