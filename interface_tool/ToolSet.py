52dd8c220850e4b7aa8efa3fa480b7077a7f0c3f
QJF <qinjinfeng@aipai.com>
2019-11-19 21:00:51 +0800

    uodate ToolSet.py

--- interface_tool/ToolSet.py
+++ interface_tool/ToolSet.py
@@ -37,25 +37,40 @@ class Tool:
             result = self.get_url(url)
             print('orderId:{} {}'.format(orderId,result['msg']))
 
-    def im_send_gift(self,bid):
-        session = login(bid)
+    def im_send_gift(self,login_bid):
+        session = self.login(login_bid)
         line = self.read_file()
         toBid = []
         for line in line:
             temp_toBid = line.strip()
             if temp_toBid != '' and temp_toBid.isdigit() and len(temp_toBid) == 11:
-            toBid.append(temp_toBid)
+                toBid.append(temp_toBid)
         for toBid in toBid:
-            url = ''
+            url = 'http://api.lieyou.com/api/gift/send?os=1&appName=lieyou&appver=3.2.0&giftType=1&roomId=0&versionCode=85&scene=1&giftId=51&appLoginBid={}&packageChannel=offical&toBid={}&giftNum=10'.format(login_bid,toBid)
             r = session.get(url=url,timeout = 60)
             if r.status_code != 200:
                 raise Exception('Network Error:{}'.format(r.status_code))
-            print('toBid:{} {}'.format(toBid,json.loads(r.content)['msg']))
+            print('toBid:{} code:{}'.format(toBid,json.loads(r.content)['code']))
+
+    def room_send_gift(self,login_bid):
+        session = self.login(login_bid)
+        line = self.read_file()
+        toBid = []
+        for line in line:
+            temp_toBid = line.strip()
+            if temp_toBid != '' and temp_toBid.isdigit() and len(temp_toBid) == 11:
+                toBid.append(temp_toBid)
+        for toBid in toBid:
+            url = 'http://api.lieyou.com/api/voice_room/send_gift?os=1&appName=lieyou&appver=3.2.0&giftType=1&signStr=7d2f29a6f1e675d01574168333&roomId=11513&versionCode=85&giftId=51&appLoginBid=10000200342&packageChannel=offical&toBid=%5B%2210000248288%22%5D&giftNum=10&sendType=2&time=1574168333'
+            r = session.get(url=url,timeout = 60)
+            if r.status_code != 200:
+                raise Exception('Network Error:{}'.format(r.status_code))
+            print('toBid:{} code:{}'.format(toBid,json.loads(r.content)['code']))
 
 if __name__=='__main__':
     tool = Tool()
     try:
-        im_send_gift(10000248288)
+        tool.im_send_gift(10000248288)
         input('Enter Pass')
     except Exception as e:
         print(e)
