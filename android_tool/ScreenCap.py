f5d67aba1a611d0c8b6958d6f158f638f9791332
qinjinfeng <qinjinfeng7@outlook.com>
2019-12-03 22:20:22 +0800

    优化截图工具

--- android_tool/ScreenCap.py
+++ android_tool/ScreenCap.py
@@ -7,13 +7,16 @@ from time import localtime,strftime
 截图文件名可以通过运行py的时候带参数修改，默认为当前时间戳
 如：python *.py sceen1
 '''
-
-get_device_state()
-if len(sys.argv)==2:
-    screencap(sys.argv[1])
-else:
-    file_name = strftime('%Y%m%d%H%M%S',localtime())
-    screencap(file_name)
+def main():
+    get_device_state()
+    if len(sys.argv)==2:
+        screencap(sys.argv[1])
+    else:
+        file_name = strftime('%Y%m%d%H%M%S',localtime())
+        screencap(file_name)
 
 if __name__=='__main__':
-    input('Enter Pass')
+    try:
+        main()
+    except Exception as e:
+        print(e)
