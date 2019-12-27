42646e97e8c24c162d7f270c88b358c71d5f94a4
qinjinfeng <qinjinfeng7@outlook.com>
2019-12-27 23:16:55 +0800

    update

--- temp/read_file_Path.py
+++ temp/read_file_Path.py
@@ -4,31 +4,47 @@ import os
 """
 获取指定文件路径下的所有文件名
 """
+# os.path.getmtime() 获取文件最后修改时间
+# os.path.getctime() 获取文件最后创建时间
+# os.path.join 拼接文件路径
+# os.path.isfile 判断是否是文件
+# os.path.isdir 判断是否是路径
 
-#os.walk 模块
-file_path = r'C:\Users\Administrator\Downloads\InstallApp'
+# os.walk 模块
+file_path = r'C:\Users\jinfengqin\Downloads\InstallApp'
 def oswalk(path):
     for root,dirs,files in os.walk(file_path):
-        print('root:',root)  #获取当前目录路径
-        print('dirs:',dirs)  #获取当前路径子目录
-        print('files:',files) #获取当前路径非子目录文件
+        print('root:',root)  # 获取当前目录路径
+        print('dirs:',dirs)  # 获取当前路径子目录
+        print('files:',files)  # 获取当前路径非子目录文件
 
-
+# os.listdir 模块
 def listdir(path):
+    file_time = 0
     for files in os.listdir(path):
         print(files)
         #print(type(files))
-        #file_path = os.path.join(path,files)
-        if os.path.isfile(file_path):
+        file_path = os.path.join(path,files)  # os.path.join 拼接文件路径
+        if os.path.isfile(file_path):  # os.path.isfile 判断是否是文件
             print('file_path:',file_path)
+            print(os.path.getmtime(file_path))
+            file_time_temp = os.path.getmtime(file_path)
+            if file_time_temp > file_time:
+                file_time = file_time_temp
+                file_name = file_path
+    print('file_name:',file_name)
+            #list_name.append(file_path)
+    #print(list_name)     
+        #if os.path.isdir(file_path):   # os.path.isdir 判断是否是路径
+        #    print('file_path1:',file_path)
         
-        if os.path.isdir(file_path):
-            print('file_path:',file_path)
-        '''
+
+                
+'''
         file_path = os.path.join(path, file)
         if os.path.isdir(file_path):
             listdir(file_path,list_name)
         else:
             list_name.append(file_path)
-        '''
+'''
 listdir(file_path)
